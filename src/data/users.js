6e4f1af2b4019bfa3414b10fedf8b2f2052f832e
Cara-VC <carakuro9@gmail.com>
2022-07-21 21:44:23 -0500

    add change user

--- src/data/users.js
+++ src/data/users.js
@@ -6,7 +6,9 @@ const saltRounds = 10;
 
 module.exports = {
     createUser,
-    getUserById
+    getUserByUsername,
+    checkUser,
+    updateUser
 };
 
 function checkUsername(username){
@@ -14,6 +16,7 @@ function checkUsername(username){
     username=username.trim();
     if(! /^[\d\w]+$/.test(username)) throw "Error: The username only include alphanumeric characters";
     if(username.length<4) throw "Error: The username should be at least 4 characters long";
+    if(username.length>16) throw "Error: The username should be no more than 16 characters long";
 //    username=username.toLowerCase();
     return username;
 }
@@ -25,8 +28,9 @@ function checkPassword(password){
     return password;
 }
 
-async function createUser(username, password, address, email, phone, ...theArgs){
+async function createUser(username, password, address, email, phone, receiveNotice, ...theArgs){
     if(!username|| !password|| !address|| !email|| !phone) throw "Error: Input Incomplete"
+    if(theArgs.length>0) throw "Number of inputs are exceed upper limit";
     username = checkUsername(username);
     password = checkPassword(password);
 
@@ -36,13 +40,12 @@ async function createUser(username, password, address, email, phone, ...theArgs)
 
     const hash = await bcrypt.hash(password, saltRounds);
     let newUsers = {
-        userId:undefined,
         username:username,
         password:hash,
         address:address,
         email:email,
         phone:phone,
-        receiveNotice:false,
+        receiveNotice:receiveNotice,
         qrCode:undefined,
         pets:[]
     };
@@ -72,3 +75,71 @@ async function getUserById(id,...theArgs){
     return findUser;
 };
 
+async function getUserByUsername(username,...theArgs){
+    if(!username) throw "You must provide an id to search for";
+    if(theArgs.length>0) throw "This function has only 1 input";
+
+    const usersCollection = await users();
+    const findUser = await usersCollection.findOne({username:{ $regex: username, $options: '$i' }});
+    if(findUser === null) throw "Error: No user with that username";
+    findUser._id =findUser._id.toString();
+    return findUser;
+};
+
+
+async function checkUser(username, password,...theArgs){
+    if(!username||!password) throw "Error: You should input both username and password";
+    if(theArgs.length>0) throw "Number of inputs are exceed upper limit";
+    username = checkUsername(username);
+    password = checkPassword(password);
+
+    const usersCollection = await users();
+    const findUser = await usersCollection.findOne({username:{ $regex: username, $options: '$i' }});
+    if(findUser === null) throw "Error: Either the username or password is invalid";
+
+    let compareToMatch = false;
+
+    try {
+        compareToMatch = await bcrypt.compare(password, findUser.password);
+    } catch (e) {
+        //no op
+    }
+
+    if (compareToMatch) {
+        return {authenticated: true};
+    } else {
+        throw "Error: Either the username or password is invalid";
+    }
+}
+
+async function updateUser (username, password, address, email, phone, receiveNotice, ...theArgs) {
+    if(!username|| !password|| !address|| !email|| !phone) throw "Error: Input Incomplete";
+    if(theArgs.length>0) throw "Number of inputs are exceed upper limit";
+    username = checkUsername(username);
+    password = checkPassword(password);
+    
+
+    const usersCollection = await users();
+    const findUser = await usersCollection.findOne({username:{ $regex: username, $options: '$i' }});
+    if (findUser === null) throw "No user with that username.";
+
+    const hash = await bcrypt.hash(password, saltRounds);
+    const qrCode=findUser.qrCode;
+    const pets=findUser.pets;
+    let updateUsers = {
+        password:hash,
+        address:address,
+        email:email,
+        phone:phone,
+        receiveNotice:receiveNotice,
+        qrCode:qrCode,
+        pets:pets
+    };
+    const updateInfo = await usersCollection.updateOne({username:{ $regex: username, $options: '$i' }}, { $set: updateUsers});
+    if (!updateInfo.matchedCount && !updateInfo.modifiedCount) {
+        throw new Error("Could not update user successfully.");
+    }
+
+    let result = await getUserByUsername(username);
+    return result;
+}
--- src/tasks/seed.js
+++ src/tasks/seed.js
@@ -2,14 +2,20 @@ const dbConnection = require('../config/mongoConnection');
 const data = require('../data');
 const users = data.users;
 
+//use "npm run seed" to add the seeds
 const main = async () => {
     const db = await dbConnection.dbConnection();
     await db.dropDatabase();
 
     const catperson = await users.createUser("catperson","123123","cat town, cat planet", "catperson@gmail.com","1234567890");
 
-//  const checkCatperson = await users.checkUser("checkCatperson","123");
-//  console.log("checkCatperson:",checkCatperson);
+    const checkCatperson = await users.checkUser("catperson","123123");
+    console.log("checkCatperson:",checkCatperson);
+
+    // const updateCatperson = await users.updateUser("catperson","123123","dog town, dog planet", "dogperson@gmail.com","1234567890")
+
+    // const getCatperson = await users.getUserByUsername("catperson");
+    // console.log("getCatperson:",getCatperson)
 
     console.log('Done seeding database');
     dbConnection.closeConnection();
